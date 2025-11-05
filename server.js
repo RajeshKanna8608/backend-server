20e4b49dc39057a723589567e3a05d66597e4780
RajeshKanna <rajeshkannark222@gmail.com>
2025-11-05 21:43:57 +0530

    add game routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const gameRoutes = require('./routes/game');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/game', gameRoutes);
 
 let db;
 
